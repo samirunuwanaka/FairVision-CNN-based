ec7a0979687794324c17e8eee9e89f39fced2da5
samirunuwanaka <samirunuwanaka@gmail.com>
2026-05-02 12:25:57 +0530

    Streamlit added successfully

--- FairVision/streamlit_app.py
+++ FairVision/streamlit_app.py
@@ -1,33 +1,44 @@
-# =========================================================
-# streamlit_app
-# =========================================================
 import streamlit as st
 import torch
 from PIL import Image
-from torchvision import transforms
 from MLModel import CNN, eval_transform
 
 ages = [
-"0-2","3-9","10-19","20-29","30-39",
-"40-49","50-59","60-69","70+"
+    "0-2","3-9","10-19","20-29","30-39",
+    "40-49","50-59","60-69","70+"
 ]
 
 model = CNN(len(ages))
-model.load_state_dict(torch.load("best_fairface_model.pt"))
+checkpoint = torch.load("best_fairface_model.pt", map_location="cpu")
+
+model.load_state_dict(checkpoint["model_state_dict"])
 model.eval()
 
 transform = eval_transform
 
 st.title("🧠 FairVision Age Classification")
 
-uploaded_file = st.file_uploader("Upload Face Image")
+uploaded_files = st.file_uploader(
+    "Upload Face Images",
+    type=["jpg", "png", "jpeg"],
+    accept_multiple_files=True
+)
 
-if uploaded_file:
-    img = Image.open(uploaded_file)
-    st.image(img)
+if uploaded_files:
+    for file in uploaded_files:
+        img = Image.open(file).convert("RGB")
+        st.image(img, caption=file.name, use_container_width=True)
 
-    x = transform(img).unsqueeze(0)
-    outputs = model(x)
-    st.write(
-        outputs
-    )
+        x = transform(img).unsqueeze(0)
+
+        with torch.no_grad():
+            outputs = model(x)
+            probs = torch.softmax(outputs, dim=1)
+
+            pred_class = torch.argmax(probs, dim=1).item()
+            confidence = probs[0][pred_class].item()
+            predicted_age = ages[pred_class]
+
+        st.write(f"🎯 **Age Group:** {predicted_age}")
+        st.write(f"📊 **Confidence:** {confidence:.2f}")
+        st.markdown("---")
